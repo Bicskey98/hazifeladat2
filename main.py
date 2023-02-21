7fd113b216a71f9d025c2483b2f7bb7af6f7eda4
B3ND112 <112851709+Bicskey98@users.noreply.github.com>
2023-02-21 17:54:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+keresztnevek = None
+while keresztnevek !='':
+  keresztnevek= input("Adj meg egy keresztnevet!")
+  print(keresztnevek)
+"""
+"""
+lista=[]
+a_betusek= input("Adj meg kis a betűvel kezdődő szavakat!")
+while a_betusek !='':
+  print(a_betusek)
+  if a_betusek == "a":
+    lista.append(a_betusek)
+  else:
+    print("gewfhw")
+  """
+
+import random
+lista=[]
+szam = random.randint(0,50)
+szam1 = random.randint(0,50)
+szam2 = random.randint(0,50)
+szam3 = random.randint(0,50)
+szam4 = random.randint(0,50)
+szam5 = random.randint(0,50)
+szam6 = random.randint(0,50)
+szam7 = random.randint(0,50)
+szam8 = random.randint(0,50)
+szam9 = random.randint(0,50)
+szam10 = random.randint(0,50)
+print(szam)
+print(szam1)
+print(szam2)
+print(szam3)
+print(szam4)
+print(szam5)
+print(szam6)
+print(szam7)
+print(szam8)
+print(szam9)
+print(szam10)
+
+if szam and szam1 and szam2 and szam3 and szam4 and szam5 and szam6 and szam7 and szam8 and szam9 and szam10 % 4==0:
+  list.append(szam)
+  print(f"A lista tartalma:{lista}")
+  
+
+
